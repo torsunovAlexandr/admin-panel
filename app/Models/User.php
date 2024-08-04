87ff5d01c37445273f9804d693f10684f4f3cb18
aleksandrtorsunov <Aleksandr.Torsunov@borkde.ru>
2024-08-04 18:40:23 +0500

    Добавил модели

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,46 +2,46 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 
-class User extends Authenticatable
+/**
+ * App\Models\User
+ *
+ * @property string $id
+ * @property string $login
+ * @property float $balance
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Payment $user
+ * @method static Builder|User query()
+ * @method static Builder|User whereId($value)
+ * @method static Builder|User whereLogin($value)
+ * @method static Builder|User whereBalance($value)
+ * @method static Builder|User whereCreatedAt($value)
+ * @method static Builder|User whereUpdatedAt($value)
+ * @method static Builder|User create(array $attributes = [])
+ * @method static Builder|User find($id, $columns = ['*'])
+ * @method static Builder|User findOrFail($id, $columns = ['*'])
+ * @method static Builder|User first($columns = ['*'])
+ * @method static Builder|User firstOrCreate(array $attributes, array $values = [])
+ * @method static Paginator paginate(int $perPage = 15, array $columns = ['*'], string $pageName = 'page', int $page = 1)
+ */
+class User extends Model
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'login',
+        'balance'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function payments(): HasMany
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(Payment::class, 'login', 'login');
     }
 }
